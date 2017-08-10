87337c204b06de2a292011912fb7ce510c27d5ae
צחי בן חמו <tzachi222@Gmail.com>
2017-08-10 09:26:49 +0300

    xZX

--- OvaidaPro/bundles/bundle.js
+++ OvaidaPro/bundles/bundle.js
@@ -23461,31 +23461,7 @@ OvadiaApp.controller('odotAdminCtrl', ['$scope',
              //   $scope.Categories = "";
            // }
         }
-
-        //$scope.SelectChange = function (item) {
-        //    $scope.Article.CategoryName = item.Name;
-        //    $scope.Article.CategoryId = item.id;
-        //}
-
-        //$scope.AddArticle = function () {
-        //    $scope.loader = true;
-        //    $http.post("/Article/AddArticle", $scope.Article)
-        //        .then(function (response) {
-        //            var ErrorCode;
-        //            try {
-        //                ErrorCode = response.data.ErrorCode;
-        //            }
-        //            catch (e) {
-        //                ErrorCode = 1;
-        //            }
-        //            if (ErrorCode == 0) {
-        //                $scope.OpenPopup("מאמר נשמר בהצלחה!", "תוכל להמשיך לערוך את המאמר");
-        //                $scope.Article = response.data.Data;
-        //                $scope.isNewArticle = false;
-        //            }
-        //            $scope.loader = false;
-        //        });
-        //}
+        
 
         $scope.GetArticle = function () {
             $scope.loader = true;
@@ -23501,6 +23477,9 @@ OvadiaApp.controller('odotAdminCtrl', ['$scope',
                     if (ErrorCode == 0) {
                         $scope.Article = response.data.Data;
                         $scope.isNewArticle = false;
+                    }
+                    else if (response.data.ErrorCode == 5) {
+                        $rootScope.LogOut();
                     }
                     else {
                         $scope.OpenPopup("שגיאה בלתי צפויה!", "נסה להתחבר מחדש, ואם הבעיה איננה נפתרת פנה למנהל האתר");
@@ -23530,6 +23509,12 @@ OvadiaApp.controller('odotAdminCtrl', ['$scope',
                     $scope.Article = response.data.Data;
                     $scope.isNewArticle = false;
                 }
+                else if (response.data.ErrorCode == 5) {
+                    $rootScope.LogOut();
+                }
+                 else {
+                        $scope.OpenPopup("שגיאה בלתי צפויה!", "נסה להתחבר מחדש, ואם הבעיה איננה נפתרת פנה למנהל האתר");
+                    }
                 $scope.loader = false;
             });
         }
