e723c638a3dcd45ffe31966d95858b9a8ca96726
צחי בן חמו <tzachi222@Gmail.com>
2017-09-12 14:43:56 +0300

    asd asd

--- OvaidaPro/bundles/bundle.js
+++ OvaidaPro/bundles/bundle.js
@@ -23661,6 +23661,7 @@ OvadiaApp.controller('donationWizardCtrl', ['$scope', '$timeout', '$http', '$roo
     '$sce',
     function ($scope, $timeout, $http, $rootScope, ngDialog, $sce) {
         var self = this;
+        $scope.currentStep = 1;
         $scope.PassStep = [false, false];
         var url = "https://kesherhk.info/Pay.aspx?id=15&value=22b2629453fb4fa68e57f508552d88e9&pId=2862&dest=1&lang=he-IL";
 
@@ -23675,6 +23676,7 @@ OvadiaApp.controller('donationWizardCtrl', ['$scope', '$timeout', '$http', '$roo
             }
 
             $scope.PassStep[0] = true;
+            $scope.currentStep = 2;
             $scope.Truma.Address = $scope.Truma.Address == null ? "" : $scope.Truma.Address;
             $scope.Truma.Email = $scope.Truma.Email == null ? "" : $scope.Truma.Email;
             $scope.Truma.Comment = $scope.Truma.Comment == null ? "" : $scope.Truma.Comment;
@@ -23685,6 +23687,19 @@ OvadiaApp.controller('donationWizardCtrl', ['$scope', '$timeout', '$http', '$roo
                 + "&adrs=" + $scope.Truma.Address + "&cmnt=" + $scope.Truma.Comment; 
             $scope.iframeUrl = $sce.trustAsResourceUrl(param);
 
+        }
+
+        $scope.StartStep = function (step) {
+            
+            switch (step) {
+                case 1:
+                    $scope.currentStep = 1;
+                    break;
+                case 2:
+                    $scope.currentStep = 2;
+                    break;
+             
+            }
         }
 
         self.init();
