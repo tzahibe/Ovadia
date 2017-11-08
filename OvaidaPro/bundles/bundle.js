2b3acad890cbf3855179d4f441578a339cbd0fad
צחי בן חמו <tzachi222@Gmail.com>
2017-11-08 20:22:05 +0200

    fixed code

--- OvaidaPro/bundles/bundle.js
+++ OvaidaPro/bundles/bundle.js
@@ -22450,6 +22450,59 @@ OvadiaApp.directive('movieDetails', function () {
         templateUrl: '/Scripts/OvadiaApp/movie-details/movie-details.html'
     }
 });
+OvadiaApp.controller('homeFixedCtrl', ['$scope', 'appServices', 'ngDialog', '$timeout',
+    function ($scope, appServices, ngDialog, $timeout) {
+
+        $scope.FormVadlidation = function () {
+            if (sendMail != null)
+                return sendMail.checkValidity();
+            return false;
+        }
+
+        $scope.AddEmail = function () {
+            if (!$scope.FormVadlidation() || $scope.email_input == null) {
+                $scope.email_error = true;
+                return;
+            }
+            var message = {
+                Email: $scope.email_input
+            }
+            appServices.AddEmail(message).then(function (response) {
+
+                if (response.data.ErrorCode == 0) {
+                    $scope.successMail = true;
+                }
+                else if (response.data.ErrorCode == 2) {
+                    $scope.email_exist_error = true;
+                }
+                else {
+                    $scope.OpenErrorPopup();
+                }
+            })
+        }
+
+        $scope.OpenErrorPopup = function () {
+            currentDialog = ngDialog.open({
+                template: 'Scripts/OvadiaApp/Admin/events-dialog/fail-message.html',
+                scope: $scope
+            });
+
+            $timeout(function () {
+                $scope.closePopup();
+            }, 800);
+        }
+    }]);
+
+
+
+OvadiaApp.directive('homeFixed', function () {
+    return {
+        restrict: 'E',
+        bindToController: true,
+        controller: 'homeFixedCtrl',
+        templateUrl: '/Scripts/OvadiaApp/home-fixed/home-fixed.html'
+    }
+});
 OvadiaApp.controller('donationScreenCtrl', ['$scope', '$interval', 'appServices', '$stateParams',
     function ($scope, $interval, appServices, $stateParams) {
         self = this;
