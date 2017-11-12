ca70086965c1b44a04c94e6d115960358dd4feaf
צחי בן חמו <tzachi222@Gmail.com>
2017-11-12 12:12:16 +0200

    das

--- OvaidaPro/Scripts/OvadiaApp/sidur-component/sidur-component.js
+++ OvaidaPro/Scripts/OvadiaApp/sidur-component/sidur-component.js
@@ -1,11 +1,28 @@
 ﻿OvadiaApp.controller('sidurComponentCtrl', ['$scope', 'appServices', 'UserAccount', '$rootScope', '$interval',
     function ($scope, appServices, UserAccount, $rootScope, $interval) {
         var self = this;
+        $scope.Categories = [];
 
         self.init = function () {
-
+            $scope.getAllCategories();
         }
 
+        $scope.getAllCategories = function () {
+            $scope.loader = true;
+            appServices.GetAllSidurCateogires().then(function (data) {
+                if (data.ErrorCode == 0) {
+                    $scope.Categories = data.Data;
+                }
+                else if (data.ErrorCode == 5) {
+                    $rootScope.LogOut();
+                }
+                else {
+                    $scope.OpenPopup("שגיאה בלתי צפויה!", "נסה להתחבר מחדש, ואם הבעיה איננה נפתרת פנה למנהל האתר");
+                }
+
+                $scope.loader = false;
+            });
+        }
 
         self.init();
 
